17149f56698888975f52808e9962f9df9169c840
Raul Cavalcante <138260357+raul-cavalcante@users.noreply.github.com>
2025-10-06 19:45:22 +0000

    add bd and env of bd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(reqest, response) {
-    response.status(200).json({ status: 'ok' })
+import database from "../../../../infra/database.js";
+
+async function status(reqest, response) {
+     const result = await database.query("SELECT 1 + 1 as sum;");
+     console.log(result.rows);
+     response.status(200).json({ status: "ok" });
 }
 
-export default status
+export default status;
